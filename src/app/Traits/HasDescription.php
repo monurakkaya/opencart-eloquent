4f5b839f5646b29433aecd3ccac9b6db34f9c1ad
Mehmet Onur Akkaya <monurakkaya@gmail.com>
2021-09-05 18:22:42 +0300

    relation conflict fix

--- src/app/Traits/HasDescription.php
+++ src/app/Traits/HasDescription.php
@@ -6,11 +6,6 @@ trait HasDescription
 {
     public function getNameAttribute()
     {
-        return $this->description->name ?? null;
-    }
-
-    public function getDescriptionAttribute()
-    {
-        return $this->description->description ?? null;
+        return $this->description->name ?? '';
     }
 }
